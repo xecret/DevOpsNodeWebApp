2abb8d3f3949b43f51b9c85540caf659d13056a5
Luca Baserga <luca.baserga@gmail.com>
2025-05-12 15:04:03 +0200

    updated jenkinsfile

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Es ist Montag bei mir.');
+  res.send('Hello FS2025 DevOps Course! Es ist Montag bei mir. Cloud Deploy auf Rernder.com');
 });
 
 app.listen(PORT, HOST);
