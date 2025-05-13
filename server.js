4a97dab34fa5676ce13f9ec6d19027c97a5b209f
Luca Baserga <luca.baserga@gmail.com>
2025-05-13 19:20:18 +0200

    updated server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Es ist Montag bei mir. Cloud Deploy auf Render.com. Docker Deeployment with GitHub Action.');
+  res.send('Hello FS2025 DevOps Course! Es ist Montag bei mir. Cloud Deploy auf Render.com. Docker Deeployment with GitHub Action. Another One.');
 });
 
 app.listen(PORT, HOST);
